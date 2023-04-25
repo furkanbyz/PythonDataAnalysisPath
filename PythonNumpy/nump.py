80cf9f545c9b8cb174571ca49bb47118795f5199
Furkan Beyaz <furkan_1582@hotmail.com>
2023-04-25 18:45:19 +0300

    commit
    commit

--- PythonNumpy/nump.py
+++ PythonNumpy/nump.py
@@ -22,10 +22,10 @@ b = np.full((4, 5), 2.3)
 
 # CONSECUTİVE NUMBERS ARRAY
 c = np.arange(0, 10)
-# c = np.arange(2,10)
-# c = np.arange(2,20,4)
+c = np.arange(2,10)
+c = np.arange(2,20,4)
 # 2den 20ye kadar +4 ekleyerek array oluşturur
-# c = np.arange(35,6, -6)
+c = np.arange(35,6, -6)
 # 35den 6ya +6 ekleyerek array oluşturur
 
 # DIVIDING INTERVAL
@@ -63,15 +63,15 @@ h = f*3
 
 # ARRAY ATTRIBUTE
 k = np.random.randint(1, 10, (4, 3, 5))
-# k = k.shape
+k = k.shape
 # x.shape, Array'in kaça kaçlık olduğunu(x,y) verir.
 # 4,3,5 çıktısını verir
 
-# k = k.ndim
+k = k.ndim
 # x.ndim, Array'in kaç boyutlu olduğunu verir.
 # kaç array, kaç kolon, kaç sütun olduğu
 
-# k = k.size
+k = k.size
 # x.size, Array'in boyutunu, kaç elemana sahip olduğunu verir.
 # 4*3*5 yani total size
 
@@ -90,7 +90,7 @@ n = n.reshape(5,2).ndim
 ### STACK
 o = np.array([1,2,3,4])
 p = np.array([5,6,7,8])
-# r = np.stack([o,p])
+r = np.stack([o,p])
 # 2ye4
 # stack, tek satırlı ve sütunsuz olan bu iki array'i alt alta birbirinin row'u şekl. birleştirir.
 s = np.stack([o,p], axis=1)
@@ -114,10 +114,10 @@ v = u[:,3]
 ### FANCY INDEXING
 y = np.arange(1,15)
 indexes = [0,2,4,6]
-# y = y[indexes]
+y = y[indexes]
 y = np.arange(12).reshape(3,4)
-# y = y[ 1:3 , 2:5 ]
-# y = y.shape
+y = y[ 1:3 , 2:5 ]
+y = y.shape
 idx1 = [1,2,0]
 idx2 = [1,0,2]
 y = y[idx1, idx2]
@@ -127,27 +127,27 @@ y[[1,3,5,7]] = 100
 y[[0,2,4,6]] = [000, 200, 400, 600]
 
 ### CONDITIONAL INDEXING
-# z = np.arange(10)
-# z = [z<5]
+z = np.arange(10)
+z = [z<5]
 # değerlerin koşulu sağlama durumunu kontrol edip true false şeklinde döndürür
-# z = z[z<5]
+z = z[z<5]
 # koşulu sağlayan değerleri array şekl döndürür
 
-# z = z[(z<3) | (z>5)]
-# z = z[(z<3) & (z>5)]
+z = z[(z<3) | (z>5)]
+z = z[(z<3) & (z>5)]
 # ile koşullar eklenebilir
 
 
 ### NUMPY SUM
 a = np.arange(1,21).reshape(4,5)
 
-# filt = a<10
-# a = [a<10]
+filt = a<10
+a = [a<10]
 # true false döndürür
-# a = a[filt]
+a = a[filt]
 # koşulu sağlayan değerleri döndürür
 
-# a =np.sum(filt)
+a =np.sum(filt)
 sum = np.sum(a)
 # arraydeki tüm değerleri toplar
 
@@ -164,12 +164,12 @@ result = np.arange(1,21).reshape(4,5)
 # print(result>0)
 # tüm elemanları kontrol edip true false değerlerini döndürür
 
-# result = np.all(result>5)
+result = np.all(result>5)
 # tüm elemanları kontrol edip tek bir true ya da false değeri döndürür
 
-# result = np.all(result>5 , axis=1)
+result = np.all(result>5 , axis=1)
 # satır bazında koşulu kontrol edip true ya da false değerlerini her bir satır için döndürür
-# result = np.all(result>5 , axis=0)
+result = np.all(result>5 , axis=0)
 # sütun bazlı kontrol eder, tüm sütunlarda koşula uymayan değer old. 4 adet false değeri döner
 
 ## all ; and gibi, any ise or gibi düşünülebilir
